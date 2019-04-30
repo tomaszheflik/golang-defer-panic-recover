0999324c5953a642fefff716389e5ba10912736c
Tomasz Heflik <tomasz.heflik@sap.com>
2019-04-30 06:32:57 +0200

    Two routines

--- routines/main.go
+++ routines/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
-
+func say(s string) {
+	var i int
+	for {
+		fmt.Printf("%s -> %d\n", s, i)
+		i++
+		time.Sleep(1*time.Second)
+	}
+}
 func main() {
 	fmt.Println("Hello GO")
+	go say("Hello from routine")
+	go say("Hello from seccond routine")
+	fmt.Println("Routine fired")
+	time.Sleep(10*time.Second)
 }
